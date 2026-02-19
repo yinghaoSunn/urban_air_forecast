bd524eef7bb0980d5e86c7afc37269e1232bf1e2
Yinghao Sun <sunyh@bu.edu>
2026-02-19 17:37:09 -0500

    Add a scolling time window for better visualization.

--- Main_milestone3.R
+++ Main_milestone3.R
@@ -16,20 +16,32 @@ targets <- targets %>%
   mutate(datetime = as.POSIXct(datetime, tz = "GMT")) %>%
   arrange(site_id, variable, datetime)
 
+# Visualization only draws a 60-day-window. Is it better?
+window_days <- 60  
+
+targets_recent <- targets %>%
+  filter(duration == "P1D") %>%
+  mutate(datetime = as.POSIXct(datetime, tz = "GMT")) %>%
+  filter(datetime >= as.POSIXct(Sys.Date() - window_days, tz = "GMT"))
+
 # get met drivers
 site_meta <- download_site_meta()
 
 met <- download_met_drivers(site_meta, past_days = 60)  # 最近60天气象作为输入变量示例
 
 # visualization - target time series
-p_targets <- targets %>%
+p_targets <- targets_recent %>%
   ggplot(aes(x = datetime, y = observation, group = site_id)) +
   geom_line(alpha = 0.6) +
   facet_grid(variable ~ site_id, scales = "free_y") +
-  labs(title = "Urban air quality targets (observations)",
+  labs(title = "Urban air quality targets (last 60 days)",
+       subtitle = paste("Generated:", format(Sys.time(), tz="GMT")),
        x = "Datetime (GMT)", y = "Observation")
 
-ggsave("outputs/milestone3_targets_timeseries.png", p_targets, width = 14, height = 10)
+ggsave(
+  sprintf("outputs/milestone3_targets_last%dd_%s.png", window_days, Sys.Date()),
+  p_targets, width = 14, height = 10
+)
 
 # visualization - drivers time series
 p_met <- met %>%
